08de2a6a93e532c8014934f1372ed7a6593abdb4
maomao0007 <skylifepic07@gmail.com>
2024-06-10 00:46:25 +0800

    Add reading, creating, and editing functionality to the budget book.

--- app.js
+++ app.js
@@ -7,23 +7,78 @@ const expenseTracker = db.expenseTracker;
 
 const port = 3000
 
+const messageHandler = require("./middlewares/message-handler");
+const errorHandler = require("./middlewares/error-handler");
+
 app.engine(".hbs", engine({ extname: ".hbs" }));
 app.set("view engine", ".hbs");
 app.set("views", "./views");
 
-// app.get('/', (req, res) => {
-//   res.send("hello")
-// })
+//用於解析 POST 請求中的 URL 編碼的表單資料
+app.use(express.urlencoded({ extended: true }));
+
+// 解析在 public 檔案裡的靜態文件目錄
+app.use(express.static("public"));
 
   app.get("/", (req, res) => {
     res.render("index");
   })
 
-app.get("/expenses", (req, res) => {
+   app.get("/", (req, res) => {
+     res.redirect("/Expense-Tracker");
+   });
+
+app.get("/Expense-Tracker", (req, res) => {
   return expenseTracker.findAll()
   .then((expenses) => res.send({ expenses}))
   .catch((err) => res.status(422).json(err))
 });
+
+app.get("/Expense-Tracker/new", (req, res) => {
+  return res.render("new")
+})
+
+app.post("/Expense-Tracker", (req, res, next) => {
+  const { name, date, category, amount } = req.body;
+  const userId = req.user.id;
+  const categoryId = req.category.id
+
+  return Expense.create({ name, date, category, amount, userId, categoryId })
+    .then(() => {
+      req.flash("success", "Added successfully");
+      res.redirect("Expense-Tracker")
+    })
+    .catch((error) => {
+      error.errorMessage = "Failed to create"
+      next(error);
+    })  
+})
+
+app.get("/Expense-Tracker/:id", (req, res, next ) => {
+  const id = req.params.id
+  const userId = req.user.id
+  const categoryId = req.category.id
+
+  return Expense.findByPk(id, {
+    attributes: ["id", "name", "date", "amount", "userId", "categoryId"],
+    raw: true,
+  })
+    .then(() => {
+    if (!expense) {
+      req.flash("error", "Data not found");
+      return res.redirect("/Expense-Tracker");
+    }
+    if (expense.userId !== userId) {
+      req.flash("error", "Unauthorized access");
+      return res.redirect("/Expense-Tracker");
+    }
+    res.render("detail", { expense });
+  })
+    .catch((error) => {
+      error.errorMessage = "Failed to load"
+      next(error);
+    })
+})
 
 app.listen(port, () => {
   console.log(`express server is running on http://localhost:${port}`);
