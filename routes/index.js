0757d23cd8377ce363c4c7e77803d85be05a70b4
maomao0007 <skylifepic07@gmail.com>
2024-06-11 14:47:55 +0800

    Implement login functionality using passport.

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,11 @@
 const express = require("express");
 const router = express.Router();
 
-// 準備引入路由模組
-const expenses = require("./expenses");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+
+const db = require('../models')
+const User = db.User
 
 router.use("/Expenses-Tracker", expenses);
 
@@ -11,5 +14,37 @@ router.get("/", (req, res) => {
   res.render("index");
 });
 
+passport.use(new LocalStrategy({ usernameField: "email" }, (username, password, done) => {
+  return User.findOne({
+    attributes: ["id", "name", "email", "password"],
+    where: { email: username },
+    raw:true
+  })
+  .then((user) => {
+  if(!user || user.password !== password) {
+    return done(null, false, { message: "Incorrect email or password."})
+  }
+  return done(null, user)
+})
+  .catch((error) => {
+  error.errorMessage = "Failed to login."
+  done(error)
+  })
+}))
+
+passport.serializeUser((user, done) => {
+  const { id, name, email } = user;
+  return done(null, { id, name, email });
+});
+
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/Expense-Tracker",
+    failureRedirect: "/login",
+    failureFlash: true,
+  })
+);
+  
 // 匯出路由器
 module.exports = router;
